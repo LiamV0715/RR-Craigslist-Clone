3df3e4a61f58f026ddf1390b46a6fe412ce922d8
Liam Vermette <Liam.vermette@gmail.com>
2024-05-15 20:28:11 -0400

    added gallery using react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 // Import data
+import React from 'react';
+import Gallery from './components/Gallery';
 
 // Import components
 import './App.css';
@@ -8,10 +10,12 @@ function App(){
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <Gallery />
       </div>
     </div>
   );
 }
+
+ 
 
 export default App;
